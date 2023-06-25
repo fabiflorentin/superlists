f9719ea2823423ea4a77fd23d6a80e06663e98dd
fabiflorentin <63799501+fabiflorentin@users.noreply.github.com>
2023-06-25 17:17:47 -0300

    Update tests.py
    Remote not required code

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -124,31 +124,4 @@ class NewVisitorTest(LiveServerTestCase):
 		## Usamos uma nova versão do nagegador para garantir que nenhuma 
 		## informação de Edith está vindo de cookies, etc
 
-'''
-		self.browser.quit()
-		self.browser = webdriver.Firefox()
-
-		# Francis acessa a página inicial. Não há sinal da lista de Edith
-		self.browser.get(self.live_server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('make a fly', page_text)
-
-		# Francis inicia uma nova lista inserindo um novo item.
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy milk')
-		inputbox.send_keys(Keys.ENTER)
-		self.wait_for_row_in_list_table('1: Buy milk')
-
-		# Francis obtém seu próprio URL exclusivo
-		francis_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
-		self.assertNotEqual( francis_list_url, edith_list_url)
-
-		# Novamente não há sinal algum da lista de Edith
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertIn('Buy milk', page_text)
-'''
-
 		# Fim
